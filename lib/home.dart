5f56a3f6fa49d3cafb39be4447f9b508b6287ce2
R K <ryukomiyama@gmail.com>
2025-09-28 16:51:02 -0400

    Fixed home screen styling

--- lib/home.dart
+++ lib/home.dart
@@ -11,33 +11,32 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       child: Center(
         child: Column(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
             children: <Widget>[
-            BasicCard(
-                title: Text("Browse recipes"),
-                leading: Icon(Icons.magnifying_glass),
-            ),
-            BasicCard(
-                title: Text("My recipes"),
-                leading: Icon(Icons.book_open)
-            ),
-            BasicCard(
-                title: Text("Start cooking session"),
-                leading: Icon(Icons.cooking_pot)
-            ),
-            BasicCard(
-                title: Text("Join with Code/Link"),
-                leading: Icon(Icons.people_ionic)
-            ),
+              Expanded(
+                child: BasicCard(
+                    title: Center(child: Basic(title: Text("Browse recipes"), leading: Icon(Icons.magnifying_glass))),
+                ).withMargin(all: 16),
+              ),
+              Expanded(
+                child: BasicCard(
+                    title: Center(child: Basic(title: Text("My recipes"), leading: Icon(Icons.book_open))),
+                ).withMargin(all: 16),
+              ),
+              Expanded(
+                child: BasicCard(
+                    title: Center(child: Basic(title: Text("Start cooking session"), leading: Icon(Icons.cooking_pot))),
+                ).withMargin(all: 16),
+              ),
+              Expanded(
+                child: BasicCard(
+                    title: Center(child: Basic(title: Text("Join with Code/Link"), leading: Icon(Icons.people_ionic))),
+                ).withMargin(all: 16),
+              ),
           ]
         )
       )
