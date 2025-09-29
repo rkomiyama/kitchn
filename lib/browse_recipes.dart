6f266a9891c700e02ebe1c8de01098b3fbc2fd4d
R K <ryukomiyama@gmail.com>
2025-09-29 00:04:10 -0400

    Added random recipes list functionality

--- lib/browse_recipes.dart
+++ lib/browse_recipes.dart
@@ -1,4 +1,17 @@
+import 'dart:convert';
+
 import 'package:arcane/arcane.dart';
+import 'package:http/http.dart' as http;
+import 'package:kitchn/auth/secrets.dart';
+
+var requestUrl = Uri.https(
+    'api.spoonacular.com',
+    'recipes/random',
+    {
+      "apiKey": spoonacularSecretKey,
+      "number": "10"
+    });
+var recipes = [];
 
 class BrowseRecipesScreen extends StatefulWidget {
   const BrowseRecipesScreen({super.key});
@@ -9,7 +22,41 @@ class BrowseRecipesScreen extends StatefulWidget {
 
 class _BrowseRecipesScreenState extends State<BrowseRecipesScreen> {
   @override
+  void initState() {
+    super.initState();
+
+    _loadRecipes();
+  }
+
+  void _loadRecipes() async {
+    http.Response response = await http.get(requestUrl);
+    setState(() {
+      final recipesList = jsonDecode(response.body);
+      recipes = recipesList['recipes'];
+    });
+  }
+
+  @override
+  void dispose() {
+    recipes = [];
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Center(child: PrimaryButton(child: Text("Go back"), onPressed: () => Arcane.pop(context),));
+    return Scaffold(
+        child: SingleChildScrollView(
+            child: Column(
+              children: <Widget>[
+                ...recipes.map((recipe) => BasicCard(
+                    leading: CardImage(image: Image.network(recipe['image'])),
+                    title: Text(recipe['title'])
+                )),
+                Center(child: PrimaryButton(child: Text("Go back"), onPressed: () => Arcane.pop(context)))
+              ]
+            )
+        )
+    );
+    // return Center(child: PrimaryButton(child: Text("Go back"), onPressed: () => Arcane.pop(context),));
   }
 }
